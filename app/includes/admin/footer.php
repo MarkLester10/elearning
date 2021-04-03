445de303fa55c546250be2a7915fe906fc606a60
Mark Lester Morta <marklester10.mlsm@gmail.com>
2021-04-03 18:27:46 +0800

    faculty, class views added

--- app/includes/admin/footer.php
+++ app/includes/admin/footer.php
@@ -1,16 +1,16 @@
 <!--Footer -->
 <footer class="main-footer">
-    <strong>Copyright &copy; <span class="text-info">2021 Faculty and Employee Association</span></strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 1.0
-    </div>
+  <strong>Copyright &copy; <span class="text-info">2021 Faculty and Employee Association</span></strong>
+  All rights reserved.
+  <div class="float-right d-none d-sm-inline-block">
+    <b>Version</b> 1.0
+  </div>
 </footer>
 <!--end footer -->
 
 <!-- Control Sidebar -->
 <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
+  <!-- Control sidebar content goes here -->
 </aside>
 <!-- /.control-sidebar -->
 </div>
@@ -22,7 +22,7 @@
 <script src="../assets/admin/js/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
-    $.widget.bridge('uibutton', $.ui.button)
+$.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
 <script src="../assets/admin/js/bootstrap.bundle.min.js"></script>
@@ -34,6 +34,13 @@
 <!--bootstrap select -->
 <script src="../assets/js/bootstrap-select.min.js"></script>
 <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script> -->
+<script src="//cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+
+<script>
+$(document).ready(function() {
+  $('#resultTbl').DataTable();
+});
+</script>
 
 
 
